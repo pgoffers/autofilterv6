512dbe45c16d5e1be07975c5a9ce75558928ee23
pgoffers <107053079+pgoffers@users.noreply.github.com>
2022-06-07 19:32:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class script(object):
     START_TXT = """Hai {}, I am Auto Filter bot 🚀 With Same more Features ✨"""
     HELP_TXT = """MORE INFORMATION IS HERE✨."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/umlinks>Umlinks</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/filmy_house>filmy_house</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -10,10 +10,10 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Auto Filter bot is a open source project. 
-- Source - https://github.com/selfie-bd/autofilterv5  
+- Source - https://t.me/filmy_house  
 
 <b>DEVS:</b>
-- <a href=https://t.me/umlinks>@UmLinks</a>"""
+- <a href=https://t.me/divyaprabha77>@divyaprabha77</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
@@ -38,7 +38,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/umlinks)</code>
+<code>[Button Text](buttonurl:https://t.me/filmy_house)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
